e52bc40f40182ea1464fc7fe0f2343f58f2e723d
Tóth Károly <toth.karolycar84@gmail.com>
2024-06-23 20:41:40 +0200

    palidrome kész

--- Palidrome/Program.cs
+++ Palidrome/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+// UI kommunikáció
+            string input;
+
+// UI kommunikáció
+            Console.WriteLine("Üdvözöllek az Palidrome játékban!");
+            Console.WriteLine("A játék lényege, hogy adj meg egy szöveget és én megmondom" +
+                " 'Palidrome' vagy sem!");
+            Console.WriteLine();
+
+            Console.WriteLine("Kérem a szöveget:");
+            input = Console.ReadLine().ToLower();
+
+            string reversed = ReverseString(input);
+
+
+            if (input == reversed)
+            {
+                Console.WriteLine("Palidrome a szöveg!");
+            }
+            else
+            {
+                Console.WriteLine("Nem Palidrome a szöveg: " + input);
+            }
+        }
+
+        static string ReverseString(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
